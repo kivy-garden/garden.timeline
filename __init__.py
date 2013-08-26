6d814945eb5af0e30f56eb6ccae4086ec19452ce
Greg <Greg@Euler>
2013-08-26 11:27:43 -0500

    added attributes :attr:`TimeTick.time_0`, :attr:`TimeTick.time_1`, :attr:`TimeTick.min_time`, and :attr:`TimeTick.max_time`. Also added more doc.

--- __init__.py
+++ __init__.py
@@ -1,3 +1,103 @@
+'''
+Timeline
+========
+
+The :class:`Timeline` is a widget specialized for displaying time
+information on a continuous, and perhaps infinite, scale. 
+It inherits from :class:`kivy.garden.tickline.Tickline` and thus allows
+zooming in and out, and panning across time. Since :class:`Timeline` works very
+much like :class:`kivy.garden.tickline.Tickline`, how 
+:class:`Timeline` works is fairly adequately covered 
+in the :mod:`kivy.garden.tickline` package. 
+
+Dependencies
+============
+
+1. kivy garden package :mod:`kivy.garden.tickline`. Use 
+    ``garden install tickline`` to install it, just like installing any
+    other garden package.
+
+2. the ``pytz`` module for handling timezones. Use ``easy_install pytz`` or
+    ``pip install pytz`` to install it for your python distribution.
+    
+3. for getting the local timezone, Windows or Unix-based systems need to get
+   the ``tzlocal`` python module. ``easy_install`` or ``pip install`` should
+   suffice here. 
+
+Platforms
+=========
+
+Most of the code here work great regardless of platform. The only exception is
+getting the local time zone.
+
+With the above dependencies installed, this is not a problem for
+desktop/laptop operating systems (Windows, Linux, OSX, etc). It should also
+work on android, which is handled through ``pyjnius``'s ``autoclass``. 
+However, it likely does NOT work in iOS. Currently
+this is not a priority, but if you'd like to have this feature, you are welcome
+to submit patches. 
+
+Using and Extending Timeline
+============================
+
+A simple timeline can be obtained by just calling::
+
+    timeline = Timeline()
+    runTouchApp(timeline)
+    
+By default, the timeline will feature ticks with intervals of
+ 1 day, 4 hours, 1 hour, 15 minutes, 5 minutes, 1 minute, 15 seconds,
+5 seconds, and 1 second which fills up the timeline adequately and not
+overwhelmingly. It will center around the current time in the current timezone.
+
+:class:`Timeline` and :class:`TimeTick` *is* timezone aware and is able to 
+handle it by themselves in most cases. They by default use the local timezone
+in the computation of times. 
+
+Most of the customizable settings in :class:`Timeline` are the same as
+:class:`~kivy.garden.tickline.Tickline`. These include 
+:attr:`~Timeline.orientation`, :attr:`~Timeline.backward`, 
+:attr:`~Timeline.line_offset`, :attr:`~Timeline.line_pos`, and
+:attr:`~Timeline.min_scale`, :attr:`~Timeline.max_scale`. 
+
+In addition, the attributes :attr:`Timeline.min_time`, :attr:`Timeline.max_time`,
+:attr:`Timeline.time_0`, and :attr:`Timeline.time_1` are given as the time
+versions of :attr:`~Timeline.min_index`, :attr:`~Timeline.max_index`,
+:attr:`~Timeline.index_0`, and :attr:`~Timeline.index_1`.
+
+The centerpiece of :class:`Timeline`, though, is really :class:`TimeTicks`s.
+There are many available options for the intervals tracked, from 1 second to
+1 day (the default ticks offer a sample of the them), listed in 
+:attr:`TimeTick.mode_options`. You can change a :class:`TimeTick`'s interval
+by changing its :attr:`~TimeTick.mode`. For example::
+
+    # interval of 1 second
+    tick = TimeTick(mode='second')
+    # interval of 15 seconds
+    tick = TimeTick(mode='15 seconds')
+    # interval of 1 minute
+    tick = TimeTick(mode='minute')
+    # interval of 30 minutes
+    tick = TimeTick(mode='30 minutes')
+    
+Most other attributes are inherited from :class:`kivy.garden.tickline.Tick`.
+These include :attr:`~TimeTick.tick_size`, :attr:`~TimeTick.label_global`,
+:attr:`~TimeTick.halign`, :attr:`~TimeTick.valign`, etc.
+
+You may use :func:`selected_time_ticks` to get a list of :class:`TimeTicks`s 
+with intervals mentioned above. Or you can call :func:`all_time_ticks` to get
+a list of :class:`TimeTick`s, one for each of available modes.
+
+A more complex working example is::
+
+
+    
+The corresponding :class:`TimeTick` overrides :meth:`Tick.tick_iter`
+to provide datetimes instead of a global index. The custom labeller
+:class:`TimeLabeller` also specializes the labelling to provide more clarity
+and space saving than the default :class:`TickLabeller`.
+
+'''
 from bisect import bisect, bisect_left
 from datetime import datetime, timedelta
 from decimal import DivisionByZero
@@ -264,8 +364,6 @@ def round_time(dt, grain='second', mode='nearest'):
 
 
 class TimeTick(Tick):  
-    second_size = [dp(1), dp(4)]
-    minute_size = [dp(2), dp(8)]
     size_dict = \
         {'day': [dp(5), dp(48)],
          '12 hours': [dp(4.5), dp(25)],
@@ -332,20 +430,20 @@ class TimeTick(Tick):
     def granularity(cls, mode):
         '''gives the multiplicity of this mode in terms of seconds.'''
         return cls.scale_factor_dict['second'] / cls.scale_factor_dict[mode]
-    def time_0(self, tickline):
-        '''gives :attr:`Tickline.index_0` in terms of time, by default
-        in the local timezone.
-        
-        :param tickline: :class:`Tickline` instance this Tick belongs to.
-        '''
-        return self.datetime_of(self.localize(tickline.index_0))
-    def time_1(self, tickline):
-        '''gives :attr:`Tickline.index_1` in terms of time, by default
-        in the local timezone.
-        
-        :param tickline: :class:`Tickline` instance this Tick belongs to.
-        '''
-        return self.datetime_of(self.localize(tickline.index_1))
+#     def time_0(self, tickline):
+#         '''gives :attr:`Tickline.index_0` in terms of time, by default
+#         in the local timezone.
+#         
+#         :param tickline: :class:`Tickline` instance this Tick belongs to.
+#         '''
+#         return self.datetime_of(self.localize(tickline.index_0))
+#     def time_1(self, tickline):
+#         '''gives :attr:`Tickline.index_1` in terms of time, by default
+#         in the local timezone.
+#         
+#         :param tickline: :class:`Tickline` instance this Tick belongs to.
+#         '''
+#         return self.datetime_of(self.localize(tickline.index_1))
     def time_min_max(self, tl, extended=False):
         '''gives either (:meth:`time_0`, :meth`time_1`) or 
         (:meth:`time_1`, :meth`time_0`) applied to ``tl``
@@ -361,10 +459,13 @@ class TimeTick(Tick):
             above. Defaults to False.
         '''
         
-        min_, max_ = [self.datetime_of(self.localize(idx)) for 
-                          idx in (
-                          (tl.index_1, tl.index_0) if tl.backward else
-                          (tl.index_0, tl.index_1))]    
+#         min_, max_ = [self.datetime_of(self.localize(idx)) for 
+#                           idx in (
+#                           (tl.index_1, tl.index_0) if tl.backward else
+#                           (tl.index_0, tl.index_1))]    
+        min_, max_ = (tl.time_1, tl.time_0) if tl.backward else \
+                        (tl.time_0, tl.time_1)
+                    
         if extended:
             interval = TimeTick.granularity(tl.densest_tick.mode)
             min_ -= timedelta(seconds=interval)
@@ -423,7 +524,7 @@ class TimeTick(Tick):
     def pos2time(self, pos, tl):
         return self.datetime_of(self.localize(tl.pos2index(pos)))
     def index_of(self, dt, global_=False):
-        '''return a localized index corresponding to a datetime. If ``global_``
+        '''return a local index corresponding to a datetime. If ``global_``
         is true, then return the global index (the index of the owning 
         :class:`Tickline`).
         
@@ -438,9 +539,6 @@ class TimeTick(Tick):
             return global_idx
         return self.localize(global_idx)
             
-#     def get_label(self, index, succinct=True, **kw):
-#         return self.get_label_texture(index, succinct, return_label=True)
-    
     def get_label_texture(self, index, succinct=True, return_kw=False,
                           return_label=False, **kw):
         if isinstance(index, Number):
@@ -466,19 +564,71 @@ class TimeTick(Tick):
         label = AutoSizeLabel(**kw)
         label.texture_update()
         return label
+    
 def all_time_ticks():
+    '''returns a list of :class:`TimeTick`s, one for each of the available
+    :attr:`~TimeTick.mode`s, specified in :attr:`TimeTick.mode_options`.
+    '''
     return [TimeTick(mode=m) for m in TimeTick.mode.options]
+
 def selected_time_ticks():
+    '''returns a list of :class:`TimeTick`s with intervals of
+    1 day, 4 hours, 1 hour, 15 minutes, 5 minutes, 1 minute, 15 seconds,
+    5 seconds, and 1 second.'''
     return [TimeTick(mode=TimeTick.mode.options[i]) for i in 
             [0, 3, 5, 7, 9, 10, 12, 14, 15]]
+    
 class Timeline(Tickline):
     labeller_cls = ObjectProperty(TimeLabeller)
-#     ticks = ListProperty([])
-    global_index = NumericProperty(
-                (local_now().astimezone(UTC) - 
-                unixepoch).days)
     tz = ObjectProperty(get_localzone())
+    def get_min_time(self, *args):
+        return self.datetime_of(self.min_index)
+    def set_min_time(self, val):
+        self.min_index = self.index_of(val)
+    min_time = AliasProperty(get_min_time, set_min_time, cache=True,
+                             bind=['min_index'])
+    '''the minimal time beyond which this :class:`Timeline` cannot go.
+    This is a time version of :attr:`Tickline.min_index`.'''
+    
+    def get_max_time(self, *args):
+        return self.datetime_of(self.max_index)
+    def set_max_time(self, val):
+        self.max_index = self.index_of(val)
+    max_time = AliasProperty(get_max_time, set_max_time, cache=True,
+                             bind=['max_index'])
+    '''the maximal time beyond which this :class:`Timeline` cannot go.
+    This is a time version of :attr:`Tickline.max_index`.'''
+        
+    def get_time_0(self, *args):
+        return self.datetime_of(self.index_0)
+    def set_time_0(self, val):
+        self.index_0 = self.datetime_of(val)
+    time_0 = AliasProperty(get_time_0, set_time_0,
+                           bind=['index_0'])  
+    '''gives the time that that sits on top of
+    ``self.x`` if :attr:`orientation` is 'vertical', or ``self.y``
+    if :attr:`orientation` is 'horizontal'. Note that this doesn't 
+    depend on :attr:`Tickline.backward`.
+    
+    This is the time version of :class:`Tickline.index_0`.'''    
+    
+    def get_time_1(self, *args):
+        return self.datetime_of(self.index_1)
+    def set_time_1(self, val):
+        self.index_1 = self.datetime_of(val)
+    time_1 = AliasProperty(get_time_1, set_time_1,
+                           bind=['index_1'])  
+    '''gives the time that that sits on top of
+    ``self.right`` if :attr:`orientation` is 'vertical', or ``self.top``
+    if :attr:`orientation` is 'horizontal'. Note that this doesn't 
+    depend on :attr:`Tickline.backward`.
+    
+    This is the time version of :class:`Tickline.index_1`.'''       
+
     def __init__(self, **kw):
+        now = local_now().astimezone(UTC)
+        self.center_on_timeframe(now - timedelta(days=1), 
+                                 now + timedelta(days=1))
         self.ticks = selected_time_ticks()
         super(Timeline, self).__init__(**kw)
     def on_tz(self, *args):
@@ -489,11 +639,7 @@ class Timeline(Tickline):
     def datetime_of(self, index):
         return (timedelta(days=index) + unixepoch).astimezone(self.tz)
     def index_of(self, dt):
-        '''return a global index corresponding to a datetime.
-        
-        :param dt: a datetime to be converted to index.
-        '''
-        
+        '''return a global index corresponding to a datetime. '''
         secs = (dt - unixepoch).total_seconds() 
         global_idx = secs / TimeTick.scale_factor_dict['second']
         return global_idx
@@ -502,31 +648,12 @@ class Timeline(Tickline):
         self.index_1 = self.index_of(end)
                 
 if __name__ == '__main__':
-#     acc = KAccordion()
-#     acc.add_widget(TimelineAccordionItem())
-#     typical_item = AccordionItem()
-#     acc.add_widget(typical_item)
-#     acc.orientation = 'vertical'
-#     acc.min_space = '80pt'
     acc = Accordion(orientation='vertical')
-    complex_ = AccordionItem(title='hello')
-#     complex_.add_widget(Tickline(ticks=[Tick(tick_size=[4, 20], offset=.5),
-#                                     Tick(scale_factor=5.),
-#                                     LabellessTick(tick_size=[1, 4],
-#                                          scale_factor=25.),
-#                                     DataListTick(data=[-0.3, 1, 1.5, 23],
-#                                                  scale_factor=5,
-#                                                  halign='line_right')],
-#                              orientation='vertical',
-#                              backward=False))
-    complex_.add_widget(Timeline(line_offset=300))
-#     complex_.add_widget(Tickline(ticks=[TimeTick(mode='second'),
-#                                     TimeTick(mode='minute'),
-#                                     TimeTick(mode='hour')
-#                                     ]))
-#     complex_.add_widget(Timeline(backward=True))
-    acc.add_widget(complex_)
-    b = BoxLayout(padding=[10, 10, 10, 10], orientation='vertical')
-    b.add_widget(acc)
-    b.add_widget(Button(text='hello'))
-    runTouchApp(b)
+    simple = AccordionItem(title='simple')
+    simple.add_widget(Timeline())
+    complex_ = AccordionItem(title='complex')
+    complex_.add_widget(
+        Timeline(orientation='horizontal',
+                 ))
+    acc.add_widget(simple)
+    runTouchApp(acc)
